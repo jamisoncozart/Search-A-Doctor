b9639890060f4ea0478073a5b30cdd00b1dd98b7
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-14 15:36:14 -0800

    Remove console.logs

--- src/betterDoctor-service.js
+++ src/betterDoctor-service.js
@@ -1,8 +1,6 @@
 export class DoctorService {
   async getDoctors(searchType, searchInput, latitude, longitude) {
-    console.log("inside DoctorService: " + latitude, longitude);
     if(latitude && longitude) {
-      //make fetch request with accurate location
       try {
         let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?${searchType}=${searchInput}&location=${latitude}%2C${longitude}%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
         if(response.ok && response.status == 200) {
