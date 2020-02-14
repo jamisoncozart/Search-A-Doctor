01dcc1cf6014bc6b4ad25588dcd043daeee9d202
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-14 09:26:22 -0800

    Add API request for symptoms

---
+++ src/betterDoctor-service.js
@@ -0,0 +1,7 @@
+export class DoctorService {
+  async getDoctorsBySymptom(symptom) {
+    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+    let jsonifiedResponse = await response.json();
+    return jsonifiedResponse;
+  }
+}
