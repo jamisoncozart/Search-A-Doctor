625a35bc82583ad240c0fd55584899b262b5d844
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-14 09:51:18 -0800

    Refactor request method to use 2 params to specify searchType and searchInput for a single request

--- src/betterDoctor-service.js
+++ src/betterDoctor-service.js
@@ -1,6 +1,6 @@
 export class DoctorService {
-  async getDoctorsBySymptom(symptom) {
-    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+  async getDoctors(searchType, searchInput) {
+    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?${searchType}=${searchInput}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
     let jsonifiedResponse = await response.json();
     return jsonifiedResponse;
   }
